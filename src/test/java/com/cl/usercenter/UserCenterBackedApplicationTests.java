e742635d49b032d5e2fa4af8fcbdec8c204418e7
Chenlong <cl1232021@outlook.com>
2024-05-21 11:03:14 +0800

    注册接口业务

--- src/test/java/com/cl/usercenter/UserCenterBackedApplicationTests.java
+++ src/test/java/com/cl/usercenter/UserCenterBackedApplicationTests.java
@@ -2,9 +2,17 @@ package com.cl.usercenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 class UserCenterBackedApplicationTests {
+
+
+    @Test
+    void testDigest(){
+        String newPassword = DigestUtils.md5DigestAsHex(("abcd" + "mypassword").getBytes());
+        System.out.println(newPassword);
+    }
 
     @Test
     void contextLoads() {
